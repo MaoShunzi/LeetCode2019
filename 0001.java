eacaa529de08d7ff46016d5eeea45193be1c7d74
MaoShunzi <32037817+MaoShunzi@users.noreply.github.com>
2019-01-19 14:45:59 +0800

    Create 0001.java
    两遍哈希表

---
+++ 0001.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map <Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<nums.length;i++) {
+            hm.put(nums[i],i);
+        }
+        for(int i=0;i<nums.length;i++) {
+            if(hm.containsKey(target-nums[i])&&hm.get(target-nums[i])!=i) {
+                return new int[] {i,hm.get(target-nums[i])};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    } 
+}
