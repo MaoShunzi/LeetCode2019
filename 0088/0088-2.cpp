2cc49c855a0777bc87ba991bf70cae65fd6d7722
MaoShunzi <32037817+MaoShunzi@users.noreply.github.com>
2019-01-26 23:07:52 +0800

    Create 0088-2.cpp
    从尾部开始比较

---
+++ 0088/0088-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int tail=m+n-1;
+        while(m>0&&n>0) {
+            if(nums1[m-1]>=nums2[n-1]) {
+                nums1[tail]=nums1[m-1];
+                m--;
+                tail--;
+            }
+            else if(nums1[m-1]<nums2[n-1]) {
+                nums1[tail]=nums2[n-1];
+                n--;
+                tail--;
+            }
+        }
+        while(n>0) {
+            nums1[n-1]=nums2[n-1];
+            n--;
+        }
+    }
+};
